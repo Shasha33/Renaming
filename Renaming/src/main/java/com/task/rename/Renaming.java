0e82fec3fa196be901f2c818932a22a547d73389
Darya Razumova <d.razumova@yandex.ru>
2019-05-20 13:45:35 +0300

    generalized renaming

--- Renaming/src/main/java/com/task/rename/Renaming.java
+++ Renaming/src/main/java/com/task/rename/Renaming.java
@@ -6,10 +6,16 @@ import java.io.*;
 import java.util.*;
 
 public class Renaming {
+    
+    private static final List<String> extensions = List.of("java", "kt");
 
-    private static boolean correctExt(String name) {
-        var extention = FilenameUtils.getExtension(name);
-        return "java".equals(extention) || "kt".equals(extention);
+    private static String newName(File file) {
+        return file.getPath() + ".2019";
+    }
+
+    private static boolean correctExtension(String name) {
+        var extension = FilenameUtils.getExtension(name);
+        return extensions.contains(extension);
     }
 
     public static List<File> rename(File directory) {
@@ -21,8 +27,8 @@ public class Renaming {
         for (var file : directory.listFiles()) {
             if (file.isDirectory()) {
                 result.addAll(rename(file));
-            } else if (correctExt(file.getName())) {
-                File renamed = new File(file.getPath() + ".2019");
+            } else if (correctExtension(file.getName())) {
+                File renamed = new File(newName(file));
 
                 if (file.renameTo(renamed)) {
                     result.add(file);
