d8ecdb96368e8904d708ac184782bda814f5bb73
Darya Razumova <d.razumova@yandex.ru>
2019-05-20 12:50:52 +0300

    main part

--- Renaming/src/main/java/com/task/rename/Main.java
+++ Renaming/src/main/java/com/task/rename/Main.java
@@ -1,2 +1,19 @@
+package com.task.rename;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+
 public class Main {
+    public static void main(String... args) {
+
+        if (args.length == 0) {
+            System.out.println("Missing argument");
+            return;
+        }
+
+        for (var file : Renaming.rename(new File(args[0]))) {
+            System.out.println(file.getName());
+        }
+    }
 }
--- Renaming/src/main/java/com/task/rename/Renaming.java
+++ Renaming/src/main/java/com/task/rename/Renaming.java
@@ -1,20 +1,34 @@
 package com.task.rename;
 
-import java.io.*;
+import com.google.common.io.*;
 
-public class Ranaming {
-    static void rename(File directory) {
+import java.io.*;
+import java.util.*;
+
+public class Renaming {
+
+    static boolean correctExt(String name) {
+        var extention = Files.getFileExtension(name);
+        return "java".equals(extention) || "kt".equals(extention);
+    }
+
+    static List<File> rename(File directory) {
         if (!directory.isDirectory()) {
-            return;
+            return new ArrayList<>();
         }
 
+        var result = new ArrayList<File>();
         for (var file : directory.listFiles()) {
             if (file.isDirectory()) {
-                rename(file);
-            } else {
-                File renamed = new File(file.getPath() + );
-                file.renameTo();
+                result.addAll(rename(file));
+            } else if (correctExt(file.getName())) {
+                File renamed = new File(file.getPath() + ".2019");
+
+                if (file.renameTo(renamed)) {
+                    result.add(file);
+                }
             }
         }
+        return result;
     }
 }
